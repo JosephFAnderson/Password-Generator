2c6fe3787b9f0a734c8aa2cd6626d6a6f5f0151b
Joseph A <joseph_anderson11@yahoo.com>
2022-09-14 21:33:05 -0400

    Create function to parse customer input and generate array of available characters

--- script.js
+++ script.js
@@ -15,6 +15,12 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   // var passLength = Number(prompt("How long would you like your password? Please enter a number between 8 and 128."));
+  var capLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  var special= ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "|", ";", ":", "'", "<", ",", ">", ".", "/", "?"];
+  var pass = "";
+
   var passLength;
   var invalid = true;
   while(invalid)
@@ -31,6 +37,21 @@ function generatePassword() {
   var passUpperCase = confirm("Would you like to use uppercase?");
   var passNumbers = confirm("Would you like to use numbers?");  
   var passSpecial = confirm("Would you like to use special characters?");
+
+  var passCriteria = [];
+  
+  function addCriteria(a, b){
+    if (a) {
+      for (var i = 0; i < b.length; i++){
+        passCriteria.push(b[i]);
+      }
+    }
+  }
+  
+  addCriteria(passLowerCase, lowerLetters);
+  addCriteria(passUpperCase, capLetters);
+  addCriteria(passNumbers, num);
+  addCriteria(passSpecial, special);
 
   return "Hello";
 }
